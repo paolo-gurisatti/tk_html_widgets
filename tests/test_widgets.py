e98389e32d4f27822a6a17fa53a8b64ddc947dc0
paolo <x>
2018-12-10 23:54:03 +0100

    Add untagged text test

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -42,6 +42,41 @@ def test_tags(Widget, f):
     assert len(text.html_parser.stack[Bind.KEY][Bind.LINK]) == 1
     root.destroy()
 
+
+@pytest.mark.parametrize("Widget", (HTMLText, HTMLScrolledText, HTMLLabel))
+def test_untagged(Widget):
+    #----------------------------------------------------------------------------------------------
+    for _ in range(10):
+        try:
+            root = tk.Tk()
+            break
+        except Exception as e:
+            exception = e
+    else:
+        raise exception
+    text = Widget(root)
+    text.pack(fill="both", expand=True)
+    text.set_html("untagged")
+    text.fit_height()
+    tags_json = json.dumps(text.html_parser._w_tags, sort_keys=True, indent=4)
+    #open(Widget.__name__ + '_untagged.json', 'w').write(tags_json)
+    assert open(Widget.__name__ + '_untagged.json', 'r').read() == tags_json
+
+    # check stack lenght
+    assert len(text.html_parser.stack[WCfg.KEY][WCfg.BACKGROUND]) == 1
+    assert len(text.html_parser.stack[WCfg.KEY][WCfg.FOREGROUND]) == 1
+    assert len(text.html_parser.stack[WCfg.KEY][WCfg.JUSTIFY]) == 1
+    assert len(text.html_parser.stack[WCfg.KEY][WCfg.TABS]) == 1
+    assert len(text.html_parser.stack[Fnt.KEY][Fnt.FAMILY]) == 1
+    assert len(text.html_parser.stack[Fnt.KEY][Fnt.SIZE]) == 1
+    assert len(text.html_parser.stack[Fnt.KEY][Fnt.WEIGHT]) == 1
+    assert len(text.html_parser.stack[Fnt.KEY][Fnt.SLANT]) == 1
+    assert len(text.html_parser.stack[Fnt.KEY][Fnt.UNDERLINE]) == 1
+    assert len(text.html_parser.stack[Fnt.KEY][Fnt.OVERSTRIKE]) == 1
+    assert len(text.html_parser.stack[Bind.KEY][Bind.LINK]) == 1
+    root.destroy()
+
+
 def test_html_format():
     #----------------------------------------------------------------------------------------------
     TEXT = '<code style="color: #%06x; background-color: #%06x; text-align: center; font-size:30px"> text color #%06x</code>'
